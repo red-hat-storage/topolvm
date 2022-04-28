4512b2bec17b04e54f712da6f657eeb0ca2db556
Yug Gupta <yuggupta27@gmail.com>
2022-04-28 20:06:45 +0530

    ext4: generate new UUID for ext4
    Signed-off-by: Yug Gupta <yuggupta27@gmail.com>

--- driver/node.go
+++ driver/node.go
@@ -211,10 +211,12 @@ func (s *nodeService) nodePublishFilesystemVolume(req *csi.NodePublishVolumeRequ
 		return status.Errorf(codes.Internal, "target device is already formatted with different filesystem: volume=%s, current=%s, new:%s", req.GetVolumeId(), fsType, mountOption.FsType)
 	}
 
-	// avoid duplicate XFS UUIDs
+	// avoid duplicate UUIDs
 	if mountOption.FsType == "xfs" {
 		mountOptions = append(mountOptions, "nouuid")
 		s.mounter.Exec.Command("xfs_admin", "-U", "generate", device)
+	} else if mountOption.FsType == "ext4" {
+		s.mounter.Exec.Command("tune2fs", "-U", "random", device)
 	}
 
 	mounted, err := filesystem.IsMounted(device, req.GetTargetPath())
